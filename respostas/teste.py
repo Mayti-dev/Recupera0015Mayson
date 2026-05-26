ca0112ab3cbe19714f23c36b87e453f9b3c0dcd7
Samuel <samuelsenac.74@gmail.com>
2026-05-26 20:30:33 -0300

    atividade 1 função

--- respostas/teste.py
+++ respostas/teste.py
@@ -1,1 +1,7 @@
-print("hello world ")
+def media (n1,n2):
+  media = (n1 + n2 ) / 2 
+  return media  
+
+n1 = float(input("DIgite a primeira nota: "))
+n2 = float(input("digite a segunda nota:")) 
+print(" a media é: ", media(n1,n2))
