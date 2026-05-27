0ce134bffff90af06a2f74929557dce953077fb7
Samuel <samuelsenac.74@gmail.com>
2026-05-26 21:30:47 -0300

    Criação exercicio 1 .py

--- respostas/teste.py
+++ respostas/teste.py
@@ -1,7 +1,12 @@
-def media (n1,n2):
-  media = (n1 + n2 ) / 2 
-  return media  
-
+aluno  = input ("digite o nome do aluno:")
 n1 = float(input("DIgite a primeira nota: "))
 n2 = float(input("digite a segunda nota:")) 
-print(" a media é: ", media(n1,n2))
+
+def calcular_media (n1,n2):
+  media = (n1 + n2 ) / 2 
+  return media 
+
+
+mediacalculada = calcular_media(n1,n2)
+
+print(f"a media do, {aluno},é: ,{mediacalculada}")
