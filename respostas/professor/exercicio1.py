e1f5e16b253ca719adc4802dbadcaf36ac065a5b
InfoSenac <informaticasenac@protonmail.com>
2026-05-26 21:35:28 -0300

    Alteração exercicio1.py

--- respostas/professor/exercicio1.py
+++ respostas/professor/exercicio1.py
@@ -1,12 +1,15 @@
+# Declaração de variáveis
 aluno = input("Digite o nome do aluno: ")
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 
+# Declaração da função
 def calcular_media(nota1, nota2):
     media = (nota1 + nota2) / 2
     return media
 
-
+# Invocando a função
 media_calculada = calcular_media(nota1=n1, nota2=n2)
 
+# Mostrando o resultado
 print(f"O aluno {aluno} obteve a seguinte média: {media_calculada}")
